295bfb42079e8bc63d2f77a97298ce5bdf385611
charlzyx <charlzyx@qq.com>
2024-01-15 16:47:45 +0800

    fix: 🐛 fix style for v5

--- scripts/patch.js
+++ scripts/patch.js
@@ -5,6 +5,38 @@ const cssPath = path.resolve(
   __dirname,
   "../node_modules/@rspress/theme-default/dist/bundle.css",
 );
+const rewite = (filePath, writer) => {
+  const old = fs.readFileSync(filePath).toString();
+  const neo = writer(old);
+  if (neo === old) return;
+  fs.writeFileSync(filePath, neo);
+};
+const loop = (filepath, it) => {
+  if (fs.existsSync(filepath)) {
+    const isDir = fs.statSync(filepath).isDirectory();
+    if (isDir) {
+      fs.readdirSync(filepath).forEach((sub) => {
+        loop(path.join(filepath, sub), it);
+      });
+    } else {
+      it(path.join(filepath));
+    }
+  }
+};
+
+const css = () => {
+  const list = [];
+  loop(path.join(__dirname, "../doc_build"), (item) => {
+    if (/\.css$/.test(item)) {
+      rewite(item, (style) => {
+        return style.replace("background-color:transparent;", "");
+      });
+    }
+  });
+  return list;
+};
+
+css();
 
 if (fs.existsSync(cssPath)) {
   const css = fs.readFileSync(cssPath, "utf-8").toString();
@@ -22,5 +54,6 @@ if (fs.existsSync(cssPath)) {
 }
 `,
   );
+
   fs.writeFileSync(cssPath, neo, "utf-8");
 }
